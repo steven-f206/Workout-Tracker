9a22de897e84b72998d8ab6682dafbd6b54dd495
Steven Ferreira <57821462+steven-f206@users.noreply.github.com>
2020-04-04 21:24:49 -0400

    second attempt at mongo pass

--- server.js
+++ server.js
@@ -1,45 +1,71 @@
-// Dependencies
+// Server Dependencies
 const express = require("express");
 const bodyParser = require('body-parser');
 const path = require('path');
+// Database Connection Request
 require('dotenv/config');
+const connectDB = require("./config/connectDB.js");
+
+//Bring in models
+const db = require("./models");
 
 // Create an instance of the express app.
 let app = express();
-const mongoose = require('mongoose');
-
 // Added so body parser can handle post requests
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // Host Static Files so css and js files can be retrieved
 app.use(express.static(path.join(__dirname, '/public')));
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 9090;
+// Set the port of our application, process.env.PORT lets the port be set by Heroku
+let PORT = process.env.PORT || 9090;
+
+
+
+
+/******************************* MiddleWare  ****************************/
+
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+app.put("/api/workouts", (req, res) => {
+    res.send(200);
+});
+
+app.post("/api/workouts", (req, res) => {
+    res.send(200);
+});
 
 /******************************* Routes  ****************************/
+
 app.get("/", (req, res) => {
-    res.send("hi");
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get("/exercise", (req, res) => {
-    res.send("hi");
+    res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
 });
 
-app.get("/api/workouts", (req, res) => {
-    res.send("hi");
-});
+    app.get("/stats", (req, res) => {
+        res.sendFile(path.join(__dirname, 'public', 'stats.html'));
+    });
 
 
-/******************************* Connect to db  ****************************/
-mongoose.connect(
-    'process.env.DB_CONNECTION',
-    { useUnifiedTopology: true, useNewUrlParser: true }, () =>
-    console.log("Connected to DB")
-);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function () {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-});
+
+
+
+    /******************************* Connect to db  ****************************/
+    connectDB()
+
+    // Start our server so that it can begin listening to client requests.
+    app.listen(PORT, function () {
+        // Log (server-side) when our server has started
+        console.log("Server listening on: http://localhost:" + PORT);
+    });
