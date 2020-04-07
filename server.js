6dd196bca5a9500772101578d2759aac877299e6
Steven Ferreira <57821462+steven-f206@users.noreply.github.com>
2020-04-06 20:55:04 -0400

    Put and get functionalities handled

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const path = require('path');
 // Database Connection Request
 require('dotenv/config');
 const connectDB = require("./config/connectDB.js");
-
 //Bring in models
 const db = require("./models");
-
 // Create an instance of the express app.
 let app = express();
 // Added so body parser can handle post requests
@@ -20,28 +18,6 @@ app.use(express.static(path.join(__dirname, '/public')));
 let PORT = process.env.PORT || 9090;
 
 
-
-
-/******************************* MiddleWare  ****************************/
-
-app.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
-
-app.put("/api/workouts", (req, res) => {
-    res.send(200);
-});
-
-app.post("/api/workouts", (req, res) => {
-    res.send(200);
-});
-
 /******************************* Routes  ****************************/
 
 app.get("/", (req, res) => {
@@ -52,20 +28,80 @@ app.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'exercise.html'));
 });
 
-    app.get("/stats", (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'stats.html'));
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'stats.html'));
+});
+
+/******************************* MiddleWare  ****************************/
+
+
+//GET REQUESTS
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).sort({ day: -1 }).limit(1)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
     });
 
+        //PUT REQUESTS
+        app.put("/api/workouts/:id", (req, res) => {
+
+            let urlData = req.params;
+            let data = req.body;
+            db.Workout.update({ _id: urlData.id }, {
+                $push: {
+                    exercises: [
+                        {
+                            "type": data.type,
+                            "name": data.name,
+                            "duration": data.duration,
+                            "weight": data.weight,
+                            "reps": data.reps,
+                            "sets": data.sets
+                        }
+                    ]
+                }
+            }).then(dbUpdate => {
+                res.json(dbUpdate);
+            })
+                .catch(err => {
+                    res.json(err);
+                });
+
+            });
+
+                //POST REQUESTS
+                /*
+                app.post("/api/workouts", (req,res) => {
+                  db.Workout.find({})
+                  .then(dbWorkout => {
+                    res.json(dbWorkout);
+                  })
+                  .catch(err => {
+                    res.json(err);
+                  });
+                });
+                */
 
 
 
-
-
-    /******************************* Connect to db  ****************************/
-    connectDB()
-
-    // Start our server so that it can begin listening to client requests.
-    app.listen(PORT, function () {
-        // Log (server-side) when our server has started
-        console.log("Server listening on: http://localhost:" + PORT);
-    });
+                /******************************* Connect to db  ****************************/
+                connectDB()
+                // Start our server so that it can begin listening to client requests.
+                app.listen(PORT, function () {
+                    // Log (server-side) when our server has started
+                    console.log("Server listening on: http://localhost:" + PORT);
+                });
